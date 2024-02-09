dc10d231d119deaaa76ada6eddb9c882ccbe9ca7
Peter A. <ink.splatters@pm.me>
2024-02-09 22:46:53 +0100

    progress

--- shell.nix
+++ shell.nix
@@ -1,14 +1,13 @@
 with import <nixpkgs> {} ;
 
-mkShell.override { stdenv = llvmPackages_17.stdenv ; } {
+mkShell.override { stdenv = llvmPackages_latest.stdenv ; } {
 
-	nativeBuildInputs = with llvmPackages_17;[
+	nativeBuildInputs = with llvmPackages_latest;[
 		clang
 		llvm
 		gnumake
 		lld
 		libtool
-		ninja
 	];
 	CFLAGS="-march=apple-m1 -mcpu=apple-m1   -mfpu=neon -mfloat-abi=hard -O3 -mllvm -vectorize-loops -flto -fprofile-generate   -fuse-ld=lld";
 	CXXFLAGS="-march=apple-m1 -mcpu=apple-m1   -mfpu=neon -mfloat-abi=hard -O3 -mllvm -vectorize-loops -flto -fprofile-generate   -fuse-ld=lld";
